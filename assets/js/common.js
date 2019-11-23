89519279f7ab9a793e47ba692509c8cb358c94b4
Kostya <pererva.kostya@outlook.com>
2019-11-23 04:12:02 +0200

    Верстка страницы Dealers

--- assets/js/common.js
+++ assets/js/common.js
@@ -90,9 +90,7 @@ if (document.querySelectorAll('.header-button-mob-menu').length) {
     moveMenu();
 }
 
-
-
-// open characteristics block - dropdown
+// open dropdown block
 
 function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
@@ -103,95 +101,169 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var Slide = function () {
-        function Slide(node, opt) {
-            _classCallCheck(this, Slide);
+    function Slide(node, opt) {
+        _classCallCheck(this, Slide);
 
-            this.opt = opt || {};
-            this.option = Object.assign({
-                btn: '[data-button]',
-                box: '[data-box-dropdown]',
-                indicator: '[data-indicator]',
-                transition: 600,
-                onOpen: null
-            }, this.opt);
-            this.node = node;
-            this.btn = this.node.querySelector(this.option.btn);
-            this.box = this.node.querySelector(this.option.box);
-            this.indicator = this.node.querySelector(this.option.indicator);
-            this.indicator.style.transition = this.option.transition + 'ms';
-            this.heightBox = null;
-            this.open = false;
-        }
-
-        _createClass(Slide, [{
-            key: "down",
-            value: function down() {
-                var _this = this;
-
-                function getSizeBoxes(elem) {
-                    var elemWidth = elem.getBoundingClientRect().right - elem.getBoundingClientRect().left;
-                    var elemHeight = elem.getBoundingClientRect().bottom - elem.getBoundingClientRect().top;
-                    return {
-                        width: elemWidth,
-                        height: elemHeight
-                    };
-                }
-
-                var downBlock = new Promise(function (resolve, reject) {
-                    _this.box.style.height = '';
-                    _this.box.style.opacity = '0';
-                    _this.box.style.display = 'block';
-                    _this.heightBox = getSizeBoxes(_this.box).height;
-                    _this.box.style.height = '0';
-                    resolve();
-                }).then(function () {
-                    setTimeout(function () {
-                        _this.box.style.transition = _this.option.transition + 'ms';
-                        _this.box.style.opacity = '1';
-                        _this.box.style.height = _this.heightBox + 'px';
-                        _this.indicator.style.transition = _this.option.transition + 'ms';
-
-                        _this.indicator.classList.add('open');
-
-                        _this.open = true;
-                    }, 0);
-                });
-            }
-        }, {
-            key: "up",
-            value: function up() {
-                var _this2 = this;
-
-                var upBlock = new Promise(function (resolve, reject) {
-                    _this2.box.style.height = '0';
-                    _this2.box.style.opacity = '0';
-
-                    _this2.indicator.classList.remove('open');
-
-                    resolve();
-                }).then(function () {
-                    setTimeout(function () {
-                        _this2.box.style.display = 'none';
-                        _this2.open = false;
-                    }, _this2.option.transition);
-                });
-            }
-        }]);
-
-        return Slide;
-    }();
-
-if (document.querySelectorAll('.product-characteristics-column').length) {
-        var slides = document.querySelectorAll('.product-characteristics-column');
-        [].forEach.call(slides, function (el) {
-            var slide = new Slide(el, {
-                transition: 400
-            });
-
-            slide.toggle = function () {
-                return slide.open ? slide.up() : slide.down();
-            };
-
-            slide.btn.addEventListener('click', slide.toggle);
-        });
+        this.opt = opt || {};
+        this.option = Object.assign({
+            btn: '[data-button]',
+            box: '[data-box-dropdown]',
+            indicator: '[data-indicator]',
+            transition: 600,
+            onOpen: null
+        }, this.opt);
+        this.node = node;
+        this.btn = this.node.querySelector(this.option.btn);
+        this.box = this.node.querySelector(this.option.box);
+        this.indicator = this.node.querySelector(this.option.indicator);
+        this.indicator.style.transition = this.option.transition + 'ms';
+        this.heightBox = null;
+        this.open = false;
     }
+
+    _createClass(Slide, [{
+        key: "down",
+        value: function down() {
+            var _this = this;
+
+            function getSizeBoxes(elem) {
+                var elemWidth = elem.getBoundingClientRect().right - elem.getBoundingClientRect().left;
+                var elemHeight = elem.getBoundingClientRect().bottom - elem.getBoundingClientRect().top;
+                return {
+                    width: elemWidth,
+                    height: elemHeight
+                };
+            }
+
+            var downBlock = new Promise(function (resolve, reject) {
+                _this.box.style.height = '';
+                _this.box.style.opacity = '0';
+                _this.box.style.display = 'block';
+                _this.heightBox = getSizeBoxes(_this.box).height;
+                _this.box.style.height = '0';
+                resolve();
+            }).then(function () {
+                setTimeout(function () {
+                    _this.box.style.transition = _this.option.transition + 'ms';
+                    _this.box.style.opacity = '1';
+                    _this.box.style.height = _this.heightBox + 'px';
+                    _this.indicator.style.transition = _this.option.transition + 'ms';
+
+                    _this.indicator.classList.add('open');
+
+                    _this.open = true;
+                }, 0);
+            });
+        }
+    }, {
+        key: "up",
+        value: function up() {
+            var _this2 = this;
+
+            var upBlock = new Promise(function (resolve, reject) {
+                _this2.box.style.height = '0';
+                _this2.box.style.opacity = '0';
+
+                _this2.indicator.classList.remove('open');
+
+                resolve();
+            }).then(function () {
+                setTimeout(function () {
+                    _this2.box.style.display = 'none';
+                    _this2.open = false;
+                }, _this2.option.transition);
+            });
+        }
+    }]);
+
+    return Slide;
+}();
+
+if(document.querySelectorAll('.product-characteristics-column').length) {
+    var slides = document.querySelectorAll('.product-characteristics-column');
+
+    [].forEach.call(slides, function (el) {
+        var slide = new Slide(el, {
+            transition: 400
+        });
+
+        slide.toggle = function () {
+            return slide.open ? slide.up() : slide.down();
+        };
+
+        slide.btn.addEventListener('click', slide.toggle);
+    });
+}
+
+//animate scroll
+var AnimateScroll = function (node, opt) {
+    var self = this;
+
+    self.opt = opt || {};
+    self.option = Object.assign({
+        indicator: '[data-indicator]',
+        link: '[data-link="anchor"]',
+        stepIndicator: 42,
+        transition: 600
+    }, self.opt);
+
+    self.node = document.querySelector(node);
+    self.indicator = self.node.querySelector(self.option.indicator);
+    self.links = self.node.querySelectorAll(self.option.link);
+
+    self.initActiveLink = function () {
+        self.indicator.style.transition = 200 + 'ms';
+
+        [].forEach.call(self.links, function (link, ind) {
+            var idSection = link.getAttribute('href');
+            var nextLink = link[ind + 1];
+            var idNextSection, nextSectionPosition;
+
+            if (nextLink) {
+                idNextSection = nextLink.getAttribute('href');
+                nextSectionPosition = $(idNextSection).offset().top;
+            }
+
+            var sectionPositionTop = $(idSection).offset().top;
+            var scroll = $(document).scrollTop();
+
+            if (sectionPositionTop <= scroll + 30 || nextSectionPosition >= scroll) {
+                $(self.option.link).removeClass('active');
+                link.classList.add('active');
+                self.indicator.style.top = ind * self.option.stepIndicator + 'px';
+            }
+        });
+    };
+
+    [].forEach.call(self.links, function (link, ind) {
+        if (link) {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                self.indicator.style.transition = self.option.transition + 'ms';
+
+                window.removeEventListener('scroll', self.initActiveLink);
+
+                var idSection = this.getAttribute('href');
+                $('html, body').animate( { scrollTop: $( idSection ).offset().top }, self.option.transition );
+                $(self.option.link).removeClass('active');
+                this.classList.add('active');
+                self.indicator.style.top = ind * self.option.stepIndicator + 'px';
+
+                setTimeout(function () {
+                    window.addEventListener('scroll', self.initActiveLink);
+                }, self.option.transition + 50);
+            })
+        }
+    });
+
+    self.initActiveLink();
+
+    window.addEventListener('scroll', self.initActiveLink);
+};
+
+if (document.querySelectorAll('.product-menu').length) {
+    var animateScroll = new AnimateScroll('.product-menu', {
+        transition: 800
+    });
+}
